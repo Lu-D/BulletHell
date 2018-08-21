f05f3a6274e833e44b8ff954aa15482235ee5616
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-08-21 16:43:49 -0500

    ported assets

---
+++ Assets/Scripts/CameraAspectScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAspectScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution((int)Screen.width, (int)Screen.height, true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
