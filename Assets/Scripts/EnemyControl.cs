9e2393bb5da855b3d8154e3b2f95c27c25a0a53a
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-08-23 16:11:24 -0500

    enemy faces player

---
+++ Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour {
+
+    public Transform target;
+    public float rotationSpeed;
+
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 relative = target.position;
+        relative.x -= transform.position.x;
+        relative.y -= transform.position.y;
+
+        float angle = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90f));
+
+    }
+}
