94b3e60c4ab9cac8d6bea3aee168fa8fdac1e7c7
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-08-25 15:24:46 -0500

    Rotation speed

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -8,6 +8,7 @@ public class EnemyControl : MonoBehaviour
     public Transform target;
     public GameObject bullet;
     public float coolDown;
+    public float rotationSpeed;
     public float projSpeed;
     public float spawnDistance;
 
@@ -25,21 +26,26 @@ public class EnemyControl : MonoBehaviour
         relative.x -= transform.position.x;
         relative.y -= transform.position.y;
 
-        float angle = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg;
-        Vector3 direction = new Vector3(0, 0, angle + 90f);
-        transform.rotation = Quaternion.Euler(direction);
-
-        FireProjectiles(direction);
+        //float angle = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg;
+        //Vector3 direction = new Vector3(0, 0, angle + 90f);
+        Quaternion lookDirection = Quaternion.LookRotation(-relative, Vector3.back);
+        lookDirection.x = 0;
+        lookDirection.y = 0;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookDirection, rotationSpeed);
+         
+        FireProjectiles();
     }
 
-    void FireProjectiles(Vector3 direction)
+    void FireProjectiles()
     {
         coolDown -= Time.deltaTime;
         if ( coolDown <= 0)
         {
             Vector3 spawnPoint = Vector3.MoveTowards(transform.position, target.position, spawnDistance);
             GameObject projectile = (GameObject)Instantiate(bullet, spawnPoint, transform.rotation);
-            projectile.GetComponent<Rigidbody2D>().velocity = projectile.transform.forward * projSpeed;
+            projectile.GetComponent<Rigidbody2D>().AddForce(transform.forward.normalized * projSpeed);
+            //Rigidbody2D instantiatedProj = projectile.GetComponent<Rigidbody2D>();
+            //instantiatedProj.velocity.normalized *= projSpeed;
             coolDown = 5;
         }
     }
