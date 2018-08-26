a57008677be2c62fa6fab38d89937f28d872894f
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-08-25 23:21:57 -0500

    Projectiles fire in direction of enemy

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -5,48 +5,48 @@ using UnityEngine;
 public class EnemyControl : MonoBehaviour
 {
 
+    public Transform BossFront;
     public Transform target;
     public GameObject bullet;
     public float coolDown;
     public float rotationSpeed;
     public float projSpeed;
     public float spawnDistance;
+    private float maxCoolDown;
 
 
     // Use this for initialization
     void Start()
     {
+        maxCoolDown = coolDown;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         Vector3 relative = target.position;
         relative.x -= transform.position.x;
         relative.y -= transform.position.y;
 
-        //float angle = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg;
-        //Vector3 direction = new Vector3(0, 0, angle + 90f);
         Quaternion lookDirection = Quaternion.LookRotation(-relative, Vector3.back);
         lookDirection.x = 0;
         lookDirection.y = 0;
         transform.rotation = Quaternion.RotateTowards(transform.rotation, lookDirection, rotationSpeed);
          
-        FireProjectiles(relative);
+        FireProjectiles();
     }
 
-    void FireProjectiles(Vector3 direction)
+    void FireProjectiles()
     {
         coolDown -= Time.deltaTime;
         if ( coolDown <= 0)
         {
-            Vector3 spawnPoint = Vector3.MoveTowards(transform.position, target.position, spawnDistance);
+            Vector3 spawnPoint = Vector3.MoveTowards(transform.position, BossFront.position, spawnDistance);
+
+            //fire Projectile
             GameObject projectile = (GameObject)Instantiate(bullet, spawnPoint, transform.rotation);
-            projectile.GetComponent<Rigidbody2D>().velocity = direction.normalized * projSpeed;
-            //Rigidbody2D instantiatedProj = projectile.GetComponent<Rigidbody2D>();
-            //instantiatedProj.velocity.normalized *= projSpeed;
-            coolDown = 5;
+            projectile.GetComponent<Rigidbody2D>().velocity = (BossFront.position - transform.position) * projSpeed;
+            coolDown = maxCoolDown;
         }
     }
 }
