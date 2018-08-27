3ebeec1f2e344a2e83bdddd0304e93329e516612
Don Choi <dongha.choi@vanderbilt.edu>
2018-08-27 16:54:35 -0500

    folders update

---
+++ Assets/Scripts/LoadResources/LoadProj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadProj : MonoBehaviour {
+
+    public GameObject[] projectiles;
+
+    // Use this for initialization
+    void Start () {
+        projectiles = Resources.LoadAll("Projectiles") as GameObject[];
+        Debug.Log(projectiles);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Projectiles/ProjList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjList : MonoBehaviour {
+
+    public GameObject[] projectiles;
+
+    // Use this for initialization
+    void Start () {
+        projectiles = Resources.LoadAll("Projectile") as GameObject[];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
