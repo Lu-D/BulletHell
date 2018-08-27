432bbbb3552a91df9092f98818c74e43d3dc6804
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-08-27 16:54:12 -0500

    Added guns

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -7,21 +7,21 @@ public class EnemyControl : MonoBehaviour
 
     public Transform BossFront;
     public Transform target;
-    public GameObject bullet;
+    //public GameObject bullet;
     public Health health;
     public int startHealth;
-    public float coolDown;
+    //public float coolDown;
     public float rotationSpeed;
-    public float projSpeed;
-    public float spawnDistance;
-    private float maxCoolDown;
+    //public float projSpeed;
+    //public float spawnDistance;
+    //private float maxCoolDown;
 
 
     // Use this for initialization
     void Start()
     {
         health = new Health(startHealth);
-        maxCoolDown = coolDown;
+        //maxCoolDown = coolDown;
     }
 
     // Update is called once per frame
@@ -36,22 +36,22 @@ public class EnemyControl : MonoBehaviour
         lookDirection.y = 0;
         transform.rotation = Quaternion.RotateTowards(transform.rotation, lookDirection, rotationSpeed);
          
-        FireProjectiles();
+        //FireProjectiles();
     }
 
-    void FireProjectiles()
-    {
-        coolDown -= Time.deltaTime;
-        if ( coolDown <= 0)
-        {
-            Vector3 spawnPoint = Vector3.MoveTowards(transform.position, BossFront.position, spawnDistance);
+    //void FireProjectiles()
+    //{
+    //    coolDown -= Time.deltaTime;
+    //    if ( coolDown <= 0)
+    //    {
+    //        Vector3 spawnPoint = Vector3.MoveTowards(transform.position, BossFront.position, spawnDistance);
 
-            //fire Projectile
-            GameObject projectile = (GameObject)Instantiate(bullet, spawnPoint, transform.rotation);
-            projectile.GetComponent<Rigidbody2D>().velocity = (BossFront.position - transform.position) * projectile.GetComponent<BProjectile>().projSpeed;
-            coolDown = maxCoolDown;
-        }
-    }
+    //        //fire Projectile
+    //        GameObject projectile = (GameObject)Instantiate(bullet, spawnPoint, transform.rotation);
+    //        projectile.GetComponent<Rigidbody2D>().velocity = (BossFront.position - transform.position) * projectile.GetComponent<BProjectile>().projSpeed;
+    //        coolDown = maxCoolDown;
+    //    }
+    //}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
